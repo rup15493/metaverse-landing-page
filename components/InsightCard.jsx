bd713a66879b290cbe489eab60cc8efc0f96920f
rup15493 <rhernandez@rayblast.com>
2023-07-24 17:21:25 -0500

    try2

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,8 +1,7 @@
 'use client';
+
 import { motion } from 'framer-motion';
 import { fadeIn } from '../utils/motion';
-
-
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
     variants={fadeIn('up', 'spring', index * 0.5, 1)}
